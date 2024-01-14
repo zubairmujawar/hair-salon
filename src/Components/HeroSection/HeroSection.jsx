d8813ec3f7c6ce52655ebedbcdbadfa4bed61f01
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-01-14 20:48:28 +0530

    Hero section completed

---
+++ src/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="herosection">
+      <h1>Hair Salon</h1>
+      <p>Style Your Hair, Style Your Life</p>
+      <div className="btn-div">
+       <a href="#"><span></span>Read More</a>
+       <a href="#"><span></span>Find Out</a>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
