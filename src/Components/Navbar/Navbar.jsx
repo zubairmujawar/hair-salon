efa1263028abba3a7c213b771de647e74a7b496f
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-01-17 22:37:13 +0530

    before starting figma flow

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 const Navbar = () => {
+
+  // let navbar = document.getElementsByClassName(".navbar")
+  // let navmunu = document.getElementsById("menu")
+  // let menuBtn = document.getElementsByClassName("menuBtn")
+
+  // const handleMenu = ()=> {
+  //   if(navbar.style.right==="-250px"){
+  //     navbar.style.right=="0"
+  //   }else{
+  //     navbar.style.right==="-250px"
+  //   }
+  // }
   return (
+    <>
     <nav className='navbar'>
       <h2>Logo</h2>
       <ul>
@@ -10,6 +23,10 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
     </nav>
+    {/* <div className="menuBtn"> 
+      <img src="../../../public/images/banner.jpg" alt="img" id='menu'  />
+    </div> */}
+    </>
   )
 }
 
